a5bb5a2fba583bb7c2974a8dad68dfb904f79c58
Pablo Umanzor A. <info@redlibre.cl>
2015-09-07 18:08:53 -0300

    Create 1Wire_Addr_finder_ds18b20.ino

---
+++ 1Wire_Addr_finder_ds18b20.ino
@@ -0,0 +1,42 @@
+#include <OneWire.h>
+
+OneWire  ds(3);  // Connect your 1-wire device to pin 3
+
+void setup(void) {
+  Serial.begin(9600);
+
+  discoverOneWireDevices();
+}
+
+void discoverOneWireDevices(void) {
+  byte i;
+  byte present = 0;
+  byte data[12];
+  byte addr[8];
+  
+  Serial.print("Looking for 1-Wire devices...\n\r");
+  while(ds.search(addr)) {
+    Serial.print("\n\rFound \'1-Wire\' device with address:\n\r");
+    for( i = 0; i < 8; i++) {
+      Serial.print("0x");
+      if (addr[i] < 16) {
+        Serial.print('0');
+      }
+      Serial.print(addr[i], HEX);
+      if (i < 7) {
+        Serial.print(", ");
+      }
+    }
+    if ( OneWire::crc8( addr, 7) != addr[7]) {
+        Serial.print("CRC is not valid!\n");
+        return;
+    }
+  }
+  Serial.print("\n\r\n\rThat's it.\r\n");
+  ds.reset_search();
+  return;
+}
+
+void loop(void) {
+  
+}
